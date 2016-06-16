aadb7b5f699849918b6406e392f85edcd0332e39
binujaya <binujaya@gmail.com>
2016-06-16 13:18:13 +0530

    Functionality added to read input file

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+var fs = require('fs');
+
 var esprima = require('esprima');
 var estraverse = require('estraverse');
 var escodegen = require('escodegen');
 
-var code = 'var answer = 42; var result = 0; if(answer>20)result=10 ;';
+app = {};
 
-var ast = esprima.parse(code);
-
-console.log('\n AST BEFORE REFACTORING: \n');
-console.log(JSON.stringify(ast, null, 4));
-
-estraverse.traverse(ast, {
-  enter : function (node, parent) {
-    if(node.type=='Identifier' && node.name=='answer'){
-      node.name = 'x';
-    }
+fs.readFile('./inputFile.js','utf8', function (err,data) {
+  if (err) {
+    throw err;
   }
+
+  var ast = esprima.parse(data);
+
+  console.log('\n AST BEFORE REFACTORING: \n');
+  console.log(JSON.stringify(ast, null, 4));
+
+  estraverse.traverse(ast, {
+    enter : function (node, parent) {
+      if(node.type=='Identifier' && node.name=='answer'){
+        node.name = 'x';
+      }
+    }
+  });
+
+  var refactoredCode = escodegen.generate(ast);
+
+  console.log('\n\n REFACTORED CODE: \n');
+  console.log(refactoredCode);
 });
-
-var refactoredCode = escodegen.generate(ast);
-
-console.log('\n\n REFACTORED CODE: \n');
-console.log(refactoredCode);
---
+++ inputFile.js
@@ -0,0 +1,21 @@
+var getGrade = function (answer) {
+  var result = '';
+  if (answer>90){
+    result = 'A';
+  }
+  else if (answer>80){
+    result = 'B';
+  }
+  else if (answe>60) {
+    result = 'C';
+  }
+  else if (answer>40) {
+    result = 'D';
+  }
+  else {
+    result = 'F';
+  }
+  return result;
+}
+
+var myGrade = getGrade(42);
