0eaf40d1f3788f68107636ba2096d65cffabdad1
binujaya <binujaya@gmail.com>
2016-06-16 09:23:10 +0530

    Console log messages modified

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var code = 'var answer = 42; var result = 0; if(answer>20)result=10 ;';
 
 var ast = esprima.parse(code);
 
+console.log('\n AST BEFORE REFACTORING: \n');
+console.log(JSON.stringify(ast, null, 4));
+
 estraverse.traverse(ast, {
   enter : function (node, parent) {
     if(node.type=='Identifier' && node.name=='answer'){
@@ -16,4 +19,5 @@ estraverse.traverse(ast, {
 
 var refactoredCode = escodegen.generate(ast);
 
-console.log(JSON.stringify(refactoredCode, null, 4));
+console.log('\n\n REFACTORED CODE: \n');
+console.log(refactoredCode);
