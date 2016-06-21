abf249c7f858f6f0926bda9589129f0c047564bb
ChathuriThilakarathna <chathurikdcmt@gmail.com>
2016-06-21 18:00:44 +0530

    Add renameMethod & renameCallee functions

--- method_simplifier/method_rename.js
+++ method_simplifier/method_rename.js
@@ -1,25 +1,48 @@
+//nodemon method_rename.js
 var esprima = require('esprima');
 var estraverse = require('estraverse');
 var escodegen = require('escodegen');
 
-var code = 'var answer = 42; var result = 0; if(answer>20)result=10 ; function f1(){} function f2(){}';
+var code = 'var answer = 42; var x = 0; if(answer>20)x=10 ; function f1(){alert("test");} function f2(){} f1();';
 
 var ast = esprima.parse(code);	
 
 //console.log('\n Befor Refactoring\n');
-//console.log(JSON.stringify(ast, null, 4));
+console.log(JSON.stringify(ast, null, 4));
+
+//var pastName, newName;
+
+function renameMethod(node){
+	var pastName, newName;
+	var newMethodName = 'RenameMethod';
+	pastName = node.id.name;
+	newName = newMethodName;
+	console.log('Method name is too short: ',node.id.name);
+	node.id.name = newMethodName;
+	renameCallee(pastName,newName);
+	return node;
+}
+
+function renameCallee(pastName,newName){
+	estraverse.traverse(ast, {
+		enter : function (node, parent) {
+			if(node.type =='ExpressionStatement' && node.expression.type=='CallExpression' && node.expression.callee.name === pastName ){
+				console.log('in second traverse');
+				node.expression.callee.name = newName;
+			}
+		}
+	});
+}	
 
 estraverse.traverse(ast, {
-  enter : function (node, parent) {
-    if(parent='FunctionDeclaration' && node.type=='Identifier' && node.name.length <= 3){
-		
-		var methodName = input();
-		node.name = methodName;
-    }
-  }
+	enter : function (node, parent) {
+		if(node.type =='FunctionDeclaration' && node.id.type=='Identifier' && node.id.name.length <= 3){
+			renameMethod(node);
+		}
+	}
 });
 
 var refactoredCode = escodegen.generate(ast);
 
 console.log('\n After Refactoring\n');
-console.log(JSON.stringify(refactoredCode, null, 4));
+console.log(JSON.stringify(refactoredCode, null, 4)); 
