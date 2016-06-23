48d3b0b9f54bd115ff93f0d04c8e831f47d34505
binujaya <binujaya@gmail.com>
2016-06-23 10:12:34 +0530

    Code moved into method extraction module

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var MethodExtraction = require('./method_extraction/index.js');
 
 var esprima = require('esprima');
 var estraverse = require('estraverse');
-var escodegen = require('escodegen');
 
 fs.readFile('./inputFile.js','utf8', function (err,data) {
   if (err) {
@@ -16,11 +15,7 @@ fs.readFile('./inputFile.js','utf8', function (err,data) {
   console.log('\n AST BEFORE REFACTORING: \n');
   console.log(JSON.stringify(ast, null, 4));
 
-  //insert refactoring module here
+  //insert refactoring modules here
   MethodExtraction(ast);
 
-  var refactoredCode = escodegen.generate(ast);
-
-  console.log('\n\n REFACTORED CODE: \n');
-  console.log(refactoredCode);
 });
--- method_extraction/index.js
+++ method_extraction/index.js
@@ -1,11 +1,16 @@
 var estraverse = require('estraverse');
+var escodegen = require('escodegen');
 
 var MethodExtraction = function (ast) {
   estraverse.traverse(ast, {
     enter : function (node, parent) {
-      //dummy code inplace of method extraction implementation
-      if(node.type=='Identifier' && node.name=='answer'){
+      if(node.type=='Identifier' && node.name=='answer'){      
+
         node.name = 'x';
+        var refactoredCode = escodegen.generate(ast);
+
+        console.log('\n\n REFACTORED CODE: \n');
+        console.log(refactoredCode);    
       }
     }
   });
