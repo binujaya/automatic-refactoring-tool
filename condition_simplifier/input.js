f398777ad4efbe2afa4eed8396162ab49d66b760
RusiruMeranga <seerusiru@gmail.com>
2016-08-06 19:46:07 +0530

    Step 1 of removing nodes from AST(hardcoded version)

--- condition_simplifier/input.js
+++ condition_simplifier/input.js
@@ -1,5 +1,3 @@
-
-
 var price=1000.0;
 
 var discount = true;
@@ -7,14 +5,12 @@ var printBill=function(){
     console.log("The total bill is :",amount);
 }
 
-
-
 if (discount) {
   amount = price * 0.90;
   printBill();
 }
-else {
-  amount = price;
-  printBill();
-}
 
+else {
+ amount = price;
+ printBill();
+}
--- condition_simplifier/rusiru.js
+++ condition_simplifier/rusiru.js
@@ -1,90 +1,40 @@
 var fs = require('fs');
 
+
+var escodegen = require('escodegen');
 var esprima = require('esprima');
 var estraverse = require('estraverse');
-var escodegen = require('escodegen');
-
-var removedNode;
-var parentNode;
 
 fs.readFile('./input.js','utf8', function (err,data) {
   if (err) {
     throw err;
   }
 
+var code = 'if (discount) { amount = price * 0.90; printBill();}';
+console.log(data);
 var ast = esprima.parse(data);
-
 console.log('\n AST BEFORE REFACTORING: \n');
 console.log(JSON.stringify(ast, null, 4));
     
-var changeNode=function(node,parent){
-    
-  
-  
-  
-    parent.callee=null;
-    parent.arguments=null;
-    parent.type=null;
-    
-    return parent;
-    
-    
-    
-};
 
-var changeAST = function (ast) {
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
+estraverse.replace(ast, {
+  enter: function enter(node) {
+    if (
+        'ExpressionStatement' === node.type
+      && 'CallExpression' === node.expression.type
+      && 'printBill' === node.expression.callee.name
+    ) {
+      return this.remove();
+    }
+  }
+});
 
-            if (node.type == 'Identifier' && node.name == 'printBill' && parent.type != 'VariableDeclarator') {
-                removedNode=node;
-                parentNode=parent;
-                changeNode(node,parent);
-                
-            
-                
-                
-            
-            }
-        }
-    });
-    
-    
-    
-    
-//    estraverse.replace(ast, {
-//        enter: function (node) {
-//
-//            if (node.type == 'Identifier' && node.name == 'printBill' ) {
-//                
-//                
-//                estraverse.VisitorOption.Remove;
-//                
-//            
-//            }
-//        }
-//    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-};
+code = escodegen.generate(ast, {
+  format: {
+    indent: { style: '  ' }
+  }
+});
 
-changeAST(ast);
-console.log("Removed node is",removedNode);
-console.log("Parent node is",parentNode);
-
-var refactoredCode = escodegen.generate(ast);
-
-console.log('\n\n REFACTORED CODE: \n');
-console.log(refactoredCode);
+console.log(code);
     
 });
