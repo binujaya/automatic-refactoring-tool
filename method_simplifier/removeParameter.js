a0da49282524559c1dd5f322b7e74b999d9ea899
ChathuriThilakarathna <chathurikdcmt@gmail.com>
2016-09-04 10:44:03 +0530

    remove unused parameters

--- method_simplifier/removeParameter.js
+++ method_simplifier/removeParameter.js
@@ -5,7 +5,7 @@ var estraverse = require('estraverse');
 var escodegen = require('escodegen');
 var jsonfile = require('jsonfile');
 
-var codeString = 'function myFunction(x, y) {return x * y; } function sumation(x,y,z){var result = x+y}';
+var codeString = 'function sumation(x,y,z){var result = x+y} function myFunction(x, y) {return x * y;} ';
 var ast = esprima.parse(codeString);	
 console.log('\n Befor Refactoring\n');
 //console.log(JSON.stringify(ast, null, 4));
@@ -27,21 +27,40 @@ function isContainedInMethodBody(parentNode, parameterName){
 	return count;
 }
 
+function removeParameter(parentNode, parameterName){
+	estraverse.replace(ast, {
+		enter: function (node,parent){
+			if ( parentNode === parent ){
+				if('Identifier' === node.type  && parameterName === node.name){
+					return this.remove();
+				}
+			}	
+		}
+	});
+}
+
 estraverse.traverse(ast, {
 	enter : function (node, parent) {
 		if(node.type =='FunctionDeclaration' && node.params.length > 0){
+			var functionName = node.id.name;
 			console.log(JSON.stringify(node.id.name)); 
 			for (var i=0; i<node.params.length; i++){
 				var para = node.params[i].name;
 				console.log(JSON.stringify(para));
 				var paraCount = isContainedInMethodBody(node,para);
 				console.log("Count :" + JSON.stringify(paraCount));
+				
+				if (paraCount == 1){
+					removeParameter(node,para);
+				}
 			}
 			
 		}
 	}
 });
 
-
+var refactoredCode = escodegen.generate(ast);
+console.log('\n After Refactoring\n');
+console.log(refactoredCode); 
 
 
