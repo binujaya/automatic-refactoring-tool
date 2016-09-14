83421c683481c30c679448382f3c817b2332bc3b
binujaya <binujaya@gmail.com>
2016-09-14 16:04:55 +0530

    unit test refined

--- method_extraction/test/methodAnalysisTest.js
+++ method_extraction/test/methodAnalysisTest.js
@@ -3,12 +3,15 @@ var esprima = require('esprima');
 
 var MethodExtraction = require('../index.js');
 
-describe('addDepthToNodes', function () {
-  beforeEach(function () {
-    ast = esprima.parse("var x = 55;");
+var sampleCode1 = "var x = 55;";
+var sampleCode2 = "var x = 45; var y = 44; var myFunc = function(x1, y1) { var z1; x1++; y1++; y1 = y1 * 2; z1 = x1 + y1; return z1; } var z = myFunc(x, y); console.log(z);";
+
+describe('addDepthToNodes', function() {
+  beforeEach(function() {
+    ast = esprima.parse(sampleCode1);
     MethodExtraction.addDepthToNodes(ast);
   })
-  it('should assign depth to each node', function () {
+  it('should assign depth to each node', function() {
     expect(ast.depth).to.equal(0);
     expect(ast.body[0].depth).to.equal(1);
   })
