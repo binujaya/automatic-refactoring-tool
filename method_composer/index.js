b735039433037933944bc6e45f5d012ea286ea7c
binujaya <binujaya@gmail.com>
2016-11-04 09:59:49 +0530

    Scope chain tracking implemented. WIP: RemoveAssingToParam

--- method_composer/index.js
+++ method_composer/index.js
@@ -1,60 +1,102 @@
 var estraverse = require('estraverse');
-var escodegen = require('escodegen');
 
-var renameMethod = function (ast, methodName, newName) {
+var scopeChain = {
+  chain: [],
+  push: function(node) {
+    this.chain.push(node);
+  },
+  pop: function() {
+    this.chain.pop();
+  },
+  print: function() {
+    console.log(this.chain.map(node => node.type).join(' => '));
+  }
+};
+
+var nameGenerator = {
+  count: 0,
+  module: {
+    comp: 'COMP',
+    cond: 'COND',
+    simp: 'SIMP'
+  },
+  genericName: function (moduleName) {
+    this.count++;
+    return 'name'+this.module[moduleName]+this.count;
+  }
+};
+
+var renameMethod = function(ast, methodName, newName) {
   estraverse.traverse(ast, {
-    enter : function (node, parent) {
-      if(node.type=='FunctionExpression' && parent.type=='VariableDeclarator' && parent.id.name==methodName){
+    enter: function(node, parent) {
+      if (node.type == 'FunctionExpression' && parent.type == 'VariableDeclarator' && parent.id.name == methodName) {
         parent.id.name = newName;
       }
     }
   });
-  var refactoredCode = escodegen.generate(ast);
-}
+};
+var printNode = function(nodeType, ast) {
+  estraverse.traverse(ast, {
+    enter: function(node) {
+      if (node.type == nodeType) {
+        console.log(node);
+      }
+    }
+  });
+};
 
 var trivialNodes = {
-  BlockStatement : 'BlockStatement',
-  VariableDeclarator : 'VariableDeclarator'
-}
+  BlockStatement: 'BlockStatement',
+  VariableDeclarator: 'VariableDeclarator'
+};
 
-var isTrivialNode = function (node) {
-  return Object.keys(trivialNodes).some(function (key) {
-    return key==node.type;
+var isTrivialNode = function(node) {
+  return Object.keys(trivialNodes).some(function(key) {
+    return key == node.type;
   });
-}
+};
 
-var addDepthToNodes = function (ast) {
+var addDepthToNodes = function(ast) {
   estraverse.traverse(ast, {
-    enter : function (node, parent) {
+    enter: function(node, parent) {
       if (!parent) node.depth = 0;
       else if (isTrivialNode(node)) node.depth = parent.depth;
       else node.depth = parent.depth + 1;
     }
   });
-}
+};
 
-var printNode = function (nodeType, ast) {
+var removeAssignToParam = function(ast) {
+  var funcParams = [];
+  var paramCount = [];
   estraverse.traverse(ast, {
-    enter : function (node) {
-      if (node.type==nodeType) {
-        console.log(node);
+    enter: function(node, parent) {
+      scopeChain.push(node);
+      if (node.type == 'FunctionExpression' && node.params.length != 0) {
+        var count = 0;
+        funcParams = node.params.map(function(param) {
+          count++;
+          return param.name;
+        });
+        paramCount.push(count);
+      }
+      if (funcParams.length != 0) {
+        if (node.type == 'AssignmentExpression' && funcParams.indexOf(node.left.name)!=-1) {
+          // TODO: rename future occurences
+          console.log('node left:',node.left);
+          console.log('node:',node.type);
+          console.log('parent:',parent.type);
+        }
+      }
+    },
+    leave: function(node, parent) {
+      scopeChain.pop();
+      if (node.type == 'FunctionExpression' && node.params.length != 0) {
+        funcParams.splice(paramCount.pop());
       }
     }
   });
-}
-
-var removeAssignToParam = function (ast) {
-  var parameters = [];
-  estraverse.traverse(ast, {
-    enter : function (node, parent) {
-      if (node.type=='FunctionExpression') {
-        parameters = node.params.map(function (param) {return param.name;});
-        console.log('params',parameters);
-        console.log(node);
-      }
-    }
-  });
-}
+};
 
 
 
