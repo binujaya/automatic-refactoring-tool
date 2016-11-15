00c98ca53733a09b2e8f7adb03d23a8688b5be99
RusiruMeranga <seerusiru@gmail.com>
2016-11-15 19:25:16 +0530

    improvements to replace nested conditionals

---
+++ condition_simplifier/scopeChain.js
@@ -0,0 +1,37 @@
+var scopeChain = {
+  chain: [],
+  blocks: [],
+  push: function(node) {
+    this.chain.push(node);
+    if (node.type == 'BlockStatement') {
+      this.blocks.push(node);
+    }
+  },
+  pop: function() {
+    var node = this.chain.pop();
+    if (node.type == 'BlockStatement') {
+      this.blocks.pop();
+    }
+  },
+  find: function(nodeType) {
+    return this.chain.find(function(node) {
+      return node.type == nodeType;
+    });
+  },
+  getCurrentBlock: function() {
+    return this.blocks[this.blocks.length - 1].body;
+  },
+  getParentBlock: function() {
+    return this.blocks[this.blocks.length - 2].body;
+  },
+  getGrandParentNode: function() {
+    return this.chain[this.chain.length - 3];
+  },
+  print: function() {
+    console.log(this.chain.map(node => node.type).join(' => '));
+  }
+};
+
+module.exports = {
+  scopeChain: scopeChain
+};
