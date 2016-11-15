08aaaaa6c1a9cc296130f1d199df6be8ed1fc7c2
RusiruMeranga <seerusiru@gmail.com>
2016-11-15 09:53:07 +0530

    replace_nested_conditionals developed to single line case

--- input/input_nested_conditionals.js
+++ input/input_nested_conditionals.js
@@ -1,20 +1,64 @@
-var getPayAmount=function() {
-  var  result;
-  if (isDead){
+//var getPayAmount=function() {
+//  var  result;
+//  if (isDead){
+//    result = deadAmount();
+//  }
+//  else {
+//    if (isSeparated){
+//      result = separatedAmount();
+//    }
+//    else {
+//      if (isRetired){
+//        result = retiredAmount();
+//      }
+//      else{
+//        result = normalPayAmount();
+//      }
+//    }
+//  }
+//  return result;
+//}
+//var a = 200;
+//if (a > 100) {
+//    console.log("high");
+//
+//} else {
+//    console.log("low");
+//}
+var getPayAmount = function () {
+if (isDead) {
     result = deadAmount();
-  }
-  else {
-    if (isSeparated){
-      result = separatedAmount();
+} else {
+    if (isSeparated) {
+        result = separatedAmount();
+    } else {
+        if (isRetired) {
+            result = retiredAmount();
+        } else {
+            result = normalPayAmount();
+        }
     }
-    else {
-      if (isRetired){
-        result = retiredAmount();
-      }
-      else{
-        result = normalPayAmount();
-      }
-    }
-  }
-  return result;
 }
+}
+//if(isDead){
+//     result = deadAmount();
+//}
+//else if(isSeparated){
+//    result = separatedAmount();
+//}
+//else{
+//    result = normalPayAmount();
+//}
+//console.log("single ifs");
+//var getPayAmount = function () {
+//    if (isDead) {
+//        return deadAmount();
+//    }
+//    if (isSeparated) {
+//        return separatedAmount();
+//    }
+//    if (isRetired) {
+//        return retiredAmount();
+//    }
+//    return normalPayAmount();
+//}
--- input/output_nested_conditionals.js
+++ input/output_nested_conditionals.js
@@ -1,17 +1,12 @@
 var getPayAmount = function () {
-    var result;
     if (isDead) {
-        result = deadAmount();
-    } else {
-        if (isSeparated) {
-            result = separatedAmount();
-        } else {
-            if (isRetired) {
-                result = retiredAmount();
-            } else {
-                result = normalPayAmount();
-            }
-        }
+        return deadAmount();
     }
-    return result;
+    if (isSeparated) {
+        return separatedAmount();
+    }
+    if (isRetired) {
+        return retiredAmount();
+    }
+    return normalPayAmount();
 };
